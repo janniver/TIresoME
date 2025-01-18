1ff22100ecb9d91a08edc27aabb28bdd65e1636f
kailashgautham <kailash.gautham@gmail.com>
2025-01-18 21:50:13 +0800

    Minor ui and ux fixes

--- TIresoME/ContentView.swift
+++ TIresoME/ContentView.swift
@@ -8,44 +8,45 @@ enum CurrentScreen {
 struct ContentView: View {
     
   @StateObject var game = GuessingGame()
-  @State private var currentScreen: CurrentScreen = .alarmClock
+    @State private var currentScreen: CurrentScreen = .wordleGame
   @State private var showStats = false
 
   var body: some View {
-      Group {
-          switch currentScreen {
-          case .alarmClock:
-              VStack {
-                  Text("Alarm Clock")
-                      .font(.title)
-                      .accessibilityAddTraits(.isHeader)
-                  Button(action: {
-                      print("Before: \(currentScreen)")
-                        self.currentScreen = .wordleGame
-                        print("After: \(currentScreen)")
-                  }) {
-                      Text("Switch Off Alarm")
-                  }
-                  .buttonStyle(.bordered)
+      switch currentScreen {
+      case .alarmClock:
+          VStack {
+              Text("Alarm Clock")
+                  .font(.title)
+                  .accessibilityAddTraits(.isHeader)
+              Button(action: {
+                  print("Before: \(currentScreen)")
+                    self.currentScreen = .wordleGame
+                    print("After: \(currentScreen)")
+              }) {
+                  Text("Switch Off Alarm")
               }
-              .frame(alignment: .top)
-              .padding([.bottom], 10)
-          case .wordleGame:
-              VStack {
-                  Text("Guess The Word")
-                      .font(.title)
-                      .accessibilityAddTraits(.isHeader)
-                  GameBoardView(game: game)
-                  KeyboardView(game: game)
-                      .padding(5)
-                  ActionBarView(
-                    showStats: $showStats,
-                    game: game
-                  )
-              }
-              .frame(alignment: .top)
-              .padding([.bottom], 10)
+              .buttonStyle(.bordered)
           }
+          Color("appBackground").edgesIgnoringSafeArea(.all)
+          .frame(alignment: .top)
+          .padding([.bottom], 10)
+      case .wordleGame:
+          VStack {
+              Text("Guess The Word")
+                  .font(.title)
+                  .foregroundStyle(.orange)
+                  .accessibilityAddTraits(.isHeader)
+              GameBoardView(game: game)
+              KeyboardView(game: game)
+                  .padding(5)
+              ActionBarView(
+                showStats: $showStats,
+                game: game
+              )
+          }
+          .frame(alignment: .top)
+          .padding([.bottom], 10)
+          .background(.black)
       }
   }
 }
--- TIresoME/GameBoardViews/CurrentGuessView.swift
+++ TIresoME/GameBoardViews/CurrentGuessView.swift
@@ -1,36 +1,3 @@
-/// Copyright (c) 2022 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
-
 import SwiftUI
 
 struct CurrentGuessView: View {
@@ -59,10 +26,17 @@ struct CurrentGuessView: View {
         Group {
           if guess.status == .invalidWord {
             Text("Word not in dictionary.")
-              .foregroundColor(.red)
-              .background(Color(UIColor.systemBackground).opacity(0.8))
+              .padding(10)
+              .foregroundColor(.black)
+              .background(.white)
+              .opacity(1)
+              .cornerRadius(10)
+              .overlay(
+                RoundedRectangle(cornerRadius: 5.0)
+                    .stroke(.white, lineWidth: 1)
+              )
               .onAppear {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                  DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
                   guess.status = .pending
                 }
               }
@@ -80,7 +54,7 @@ struct CurrentGuessView_Previews: PreviewProvider {
     
     let guess = Guess(
       word: [guessedLetter, guessedLetter2],
-      status: .pending
+      status: .invalidWord
     )
     CurrentGuessView(
       guess: .constant(guess),
