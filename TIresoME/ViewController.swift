2cd7bc31dde815666b637f90e93183b52e616f3c
kailashgautham <kailash.gautham@gmail.com>
2025-01-18 14:51:33 +0800

    Template commit

---
+++ TIresoME/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TIresoME
+//
+//  Created by Kailash Gautham on 18/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TIresoMETests/TIresoMETests.swift
@@ -0,0 +1,17 @@
+//
+//  TIresoMETests.swift
+//  TIresoMETests
+//
+//  Created by Kailash Gautham on 18/1/25.
+//
+
+import Testing
+@testable import TIresoME
+
+struct TIresoMETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
