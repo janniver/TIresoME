b1219609e2256b040222b01063d2b8381f89eddc
kailashgautham <kailash.gautham@gmail.com>
2025-01-18 15:27:15 +0800

    Replace template with SwiftUI

---
+++ TIresoME/TIresoMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  TIresoMEApp.swift
+//  TIresoME
+//
+//  Created by Kailash Gautham on 18/1/25.
+//
+
+import SwiftUI
+
+@main
+struct TIresoMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
